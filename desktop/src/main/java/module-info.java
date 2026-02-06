662ca301ec1664eaa0201e22930f11a76abc6f6d
ndominkiewicz <norbertd1410@gmail.com>
2026-02-06 17:36:36 +0100

    build: add controller exporting to fxml

--- desktop/src/main/java/module-info.java
+++ desktop/src/main/java/module-info.java
@@ -3,7 +3,11 @@ module com.chordly.desktop {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.fontawesome5;
+    requires org.kordamp.ikonli.javafx;
 
     opens com.chordly.desktop to javafx.fxml;
     exports com.chordly.desktop;
+    opens com.chordly.desktop.controller to javafx.fxml;
+    exports com.chordly.desktop.controller;
 }
